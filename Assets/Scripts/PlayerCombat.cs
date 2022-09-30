67f91718842ec21266c59feaa574f6edb1f4cb74
h-cao140828 <nam.cao140828@gmail.com>
2022-09-30 12:47:51 -0700

    Add attack and update Unity to 2021.3.11f1

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -12,16 +12,7 @@ public class PlayerCombat : MonoBehaviour
     GameObject currentWeaponInHand;
     GameObject currentWeaponInSheath;
     public Animator animator;
-    bool combat = false;
-    bool draw = false;
     bool sheathed = true;
-
-    // Melee combo related variables
-    public float cooldownTime = 2f;
-    private float nextFireTime = 0f;
-    public static int noOfClicks = 0;
-    float lastClickedTime = 0f;
-    float maxComboDelay = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -47,26 +38,15 @@ public class PlayerCombat : MonoBehaviour
                     SheathWeapon();
                 }
             }
-            // inside loop check if sheathed
-            // if yes, draw sword
-            // else, sheath sword
-            // outside loop do combat stuff like attack handles melee
-        //    // Enter combat if "T" key was pressed
-        //    if (keyboard.tKey.wasPressedThisFrame && !draw)
-        //    {
-        //        DrawWeapon();
-        //    }
-        //    else
-        //    {
-        //        SheathWeapon();
-        //    }
+
+
+            // Perform an attack if the player's weapon is drawn and left clicks
+            var mouse = Mouse.current;
+            if (mouse.leftButton.wasPressedThisFrame && !sheathed)
+            {
+                animator.SetTrigger("Attack");
+            }
         }
-
-        //if (combat)
-        //{
-        //    MeleeAttack();
-        //}
-
     }
 
     void Attack()
@@ -80,34 +60,7 @@ public class PlayerCombat : MonoBehaviour
     // A function for using melee attacks
     void MeleeAttack()
     {
-        // Check if player stop clicking and ends animation
-        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && animator.GetCurrentAnimatorStateInfo(0).IsName("hit1"))
-        {
-            animator.SetBool("hit1", false);
-        }
-        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && animator.GetCurrentAnimatorStateInfo(0).IsName("hit2"))
-        {
-            animator.SetBool("hit2", false);
-        }
-        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && animator.GetCurrentAnimatorStateInfo(0).IsName("hit3"))
-        {
-            animator.SetBool("hit3", false);
-            noOfClicks = 0;
-        }
-
-        // Check for cooldown 
-        if (Time.time - lastClickedTime > maxComboDelay)
-        {
-            noOfClicks = 0;
-        }
-        if (Time.time > nextFireTime)
-        {
-            var mouse = Mouse.current;
-            if (mouse.leftButton.wasPressedThisFrame)
-            {
-                OnClick();
-            }
-        }
+       
     }
 
     // A function for using player skills
@@ -119,74 +72,23 @@ public class PlayerCombat : MonoBehaviour
         //draw = true;
         sheathed = false;
         // Play an animation
-        animator.SetTrigger("drawWeapon");
+        animator.SetTrigger("DrawWeapon");
         // Place weapon in hand
         currentWeaponInHand = Instantiate(weapon, weaponHolder.transform);
         // Destroy the weapon in sheath
         Destroy(currentWeaponInSheath);
-
-        // Enter combat
-        //EnterCombat();
     }
 
     public void SheathWeapon()
     {
         sheathed = true;
         // Play an animation
-        animator.SetTrigger("sheathWeapon");
+        animator.SetTrigger("SheathWeapon");
         // Place weapon in sheath
         currentWeaponInSheath = Instantiate(weapon, weaponSheath.transform);
         // Destroy weapon in hand
         Destroy(currentWeaponInHand);
 
-        // Exit combat
-        //ExitCombat();
     }
 
-    // Attacks and mechanics relating to combat should be called here
-    public void EnterCombat()
-    {
-        combat = true;
-        // attack
-
-
-        var keyboard = Keyboard.current;
-        if (keyboard.tKey.wasPressedThisFrame)
-        {
-            SheathWeapon();
-        }
-    }
-
-    public void ExitCombat()
-    {
-        combat = false;
-
-    }
-
-    void OnClick()
-    {
-        // Play an attack animation
-        lastClickedTime = Time.time;
-        noOfClicks++;
-        if (noOfClicks == 1)
-        {
-            animator.SetBool("hit", true);
-        }
-        noOfClicks = Mathf.Clamp(noOfClicks, 0, 3);
-
-        if (noOfClicks >= 2 && animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && animator.GetCurrentAnimatorStateInfo(0).IsName("hit1"))
-        {
-            animator.SetBool("hit1", false);
-            animator.SetBool("hit2", true);
-        }
-
-        if (noOfClicks >= 3 && animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && animator.GetCurrentAnimatorStateInfo(0).IsName("hit2"))
-        {
-            animator.SetBool("hit2", false);
-            animator.SetBool("hit3", true);
-        }
-
-        // Detect enemies in range of attack
-        // Damage them
-    }
 }
