4eafee062d2cd59837143f5883b5b6b6c5d0c69d
h-cao140828 <nam.cao140828@gmail.com>
2022-10-09 14:12:55 -0700

    Rename damage colliders

---
+++ Assets/Scripts/DamageEnemyCollider.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageEnemyCollider : MonoBehaviour
+{
+    PlayerCombat playerCombat;
+
+    bool hasCollide = false;
+
+    public int currentWeaponDamage = 25;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameObject.transform.root.CompareTag("Player"))
+        {
+            playerCombat = gameObject.transform.root.GetComponentInChildren<PlayerCombat>();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy" && playerCombat.isAttacking && !hasCollide)
+        {
+            hasCollide = true;
+
+            EnemyStats enemyStats = other.GetComponent<EnemyStats>();
+
+            if (enemyStats != null)
+            {
+                enemyStats.TakeDamage(currentWeaponDamage);
+            }
+        }
+
+    }
+
+    private void LateUpdate()
+    {
+        ResetHasCollider();
+    }
+
+    public void ResetHasCollider()
+    {
+        hasCollide = false;
+    }
+}
