180ebba274c400a84f98d2d0b4fb13eb00e1a681
h-cao140828 <nam.cao140828@gmail.com>
2022-10-08 20:51:16 -0700

    Add enemy stats

--- Assets/Scripts/DamageCollider.cs
+++ Assets/Scripts/DamageCollider.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class DamageCollider : MonoBehaviour
 {
     PlayerCombat playerCombat;
-    //EnemyController enemy;
+    EnemyController enemy;
 
-    //public GameObject hitParticle;
     bool hasCollide = false;
+
+    public int currentWeaponDamage = 25;
 
     // Start is called before the first frame update
     void Start()
@@ -22,20 +23,52 @@ public class DamageCollider : MonoBehaviour
         if (gameObject.transform.root.CompareTag("Enemy"))
         {
             //enemy = gameObject.transform.root.GetComponentInChildren<EnemyController>();
+            enemy = gameObject.transform.root.GetComponent<EnemyController>();
         }
     }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag == "Enemy" && playerCombat.isAttacking && !hasCollide)
+    //    {
+    //        Debug.Log(other.name);
+    //        hasCollide = true;
+
+    //        other.GetComponent<Animator>().SetTrigger("Hit");
+
+    //    }
+    //}
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Enemy" && playerCombat.isAttacking && !hasCollide)
         {
-            Debug.Log(other.name);
+            Debug.Log("Enemy hi");
             hasCollide = true;
 
-            other.GetComponent<Animator>().SetTrigger("Hit");
-            // for hit effects
-            // Instantiate(hitParticle, new Vector3(other.transform.position.x, other.transform.position.y, other.transform.position.z), other.transform.rotation);
+            EnemyStats enemyStats = other.GetComponent<EnemyStats>();
+
+            if (enemyStats != null)
+            {
+                Debug.Log("Enemy got");
+                enemyStats.TakeDamage(currentWeaponDamage);
+            }
         }
+
+        // enemy controller doesn't exist b/c weapon not attached to an enemy so may have to do in separate classes cause enemies can be of different types
+        //if (other.tag == "Player" && enemy.isAttacking && !hasCollide)
+        //{
+        //    Debug.Log("player hi");
+        //    hasCollide = true;
+
+        //    PlayerStats playerStats = other.GetComponent<PlayerStats>();
+
+        //    if (playerStats != null)
+        //    {
+        //        Debug.Log("player got");
+        //        playerStats.TakeDamage(currentWeaponDamage);
+        //    }
+        //}
     }
 
     private void LateUpdate()
---
+++ Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    public int healthLevel = 10;
+    public int maxHealth;
+    public int currentHealth;
+
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        maxHealth = SetMaxHealthFromHealthLevel();
+        currentHealth = maxHealth;
+    }
+
+    int SetMaxHealthFromHealthLevel()
+    {
+        maxHealth = healthLevel * 10;
+        return maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        animator.SetTrigger("Damage");
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            animator.SetBool("IsDead", true);
+            // handle death
+        }
+    }
+}
