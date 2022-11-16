6d3760e1e6647c82c5fe03a2b785d1bb60c51929
h-cao140828 <nam.cao140828@gmail.com>
2022-11-15 17:28:03 -0800

    Update ThirdPersonController

--- Assets/Scripts/Player/ThirdPersonController.cs
+++ Assets/Scripts/Player/ThirdPersonController.cs
@@ -187,7 +187,7 @@ namespace StarterAssets
 
         private void LateUpdate()
         {
-            if (!openStats)
+            if (!openStats && !PauseMenu.gamePaused)
                 CameraRotation();
         }
 
